67c06b48bfc14d97426d93659d8507c162a11043
josephadogeriopao <joseph.adogeri@orleansassessors.com>
2025-07-31 10:59:09 -0500

    Implemented Record class

---
+++ vital-records-generator/src/models/Record.ts
@@ -0,0 +1,56 @@
+class Record {
+    private ssn : string = "";
+    private owner: string = "";
+    private rescode: string = "";
+    private address: string = "";
+    private city: string = "";
+    private dob: string = "";
+    private dod: string = "";
+    private sex: string = "";
+    private age: string = "";
+
+    constructor(){ }
+    setSSN = (ssn: string) => this.ssn = ssn;
+
+    setOwner = (owner: string) => this.owner = owner;
+
+    setResCode = (rescode: string) => this.rescode = rescode;
+
+    setAddress = (address: string) => this.address = address;
+
+    setCity = (city: string) => this.city = city;
+
+    setDOB = (dob: string) => this.dob = dob;
+
+    setDOD = (dod: string) => this.dod = dod;
+
+    setSex = (sex: string) => this.sex = sex;
+
+    setAge = (age: string) => this.age = age;
+
+    getSSN = () : string => this.ssn;
+
+    getOwner = () : string => this.owner;
+
+    getResCode = () : string => this.rescode;
+
+    getAddress = () : string => this.address;
+
+    getCity = () : string => this.city;
+
+    getDOB = () : string => this.dob;
+
+    getDOD = () : string => this.dod;
+
+    getSex = () : string => this.sex;
+
+    getAge = () : string => this.age;
+
+    toArray = () : string[] => {
+        return [this.getSSN(),this.getOwner(),this.getResCode(),this.getAddress(),
+                this.getCity(),this.getDOB(),this.getDOD(),this.getSex(),this.getAge()
+        ]
+    }
+}
+
+export default Record;
