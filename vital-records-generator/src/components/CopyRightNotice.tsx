7f2672a06c9e76b6f8d420845d75833985480fec
joseph adogeri <70236136+jadogeri@users.noreply.github.com>
2025-08-02 20:31:51 -0500

    Updated footer text

---
+++ vital-records-generator/src/components/CopyRightNotice.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const CopyrightNotice = () => {
+  const currentYear = new Date().getFullYear();
+  const copyrightHolder = "Orleans Parish Assessor's Office"; // Replace with your company or individual name
+
+  return (
+    <p style={{ textAlign: 'center', padding: '20px', fontSize: '0.9em' }}>
+      &copy; {currentYear} {copyrightHolder}. All rights reserved.
+    </p>
+  );
+};
+
+export default CopyrightNotice;
--- vital-records-generator/src/layouts/Footer.tsx
+++ vital-records-generator/src/layouts/Footer.tsx
@@ -1,20 +1,12 @@
 import React from 'react'
+import CopyRightNotice from "../components/CopyRightNotice"
 
 const Footer = () => {
     
   return (
   <footer className="w3-center w3-black w3-padding-32 w3-bottom">
-
-    <p>
-      Powered by{" "}
-      <a
-        href="https://www.w3schools.com/w3css/default.asp"
-        title="W3.CSS"
-        target="_blank"
-        className="w3-hover-text-green"
-      >
-        w3.css
-      </a>
+    <p >
+      <CopyRightNotice />
     </p>
   </footer>
   )
